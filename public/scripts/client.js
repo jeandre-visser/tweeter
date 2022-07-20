3a4111c7b9600bfbc2da5938368d094ccc3250d3
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-20 22:33:49 +0000

    Separated functions and the DOM, and added event listener for submit to prevents its default behavior

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,48 +1,56 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Functions 
+
+// Takes a tweet object and returns article element with the composed tweet
+const createTweetElement = function(tweetData) {
+  // Tweet article element that gets returned
+  const $tweet = $('<article>').addClass('tweet');
+  
+  const timeSinceTweet = (Date.now() - tweetData.created_at) / 86400000;
+
+// Template literal, the tweets inner HTML
+  const htmlTemplate = `
+    <header>
+      <img src=${tweetData.user.avatars} alt="${tweetData.user.handle}-avatar">
+      <span class="name" >${tweetData.user.name}</span>
+      <span class="handle" >${tweetData.user.handle}</span>
+    </header>
+    <p>${tweetData.content.text}</p>
+    <footer>
+      ${Math.round(timeSinceTweet)} days ago
+      <span>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </span>
+    </footer>
+  `
+  // Input the inner HTML into the tweet and return it
+  return $tweet.html(htmlTemplate);
+};
+
+
+// Takes in an array of tweet objects and then appends each one to the #tweets-container
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    $('#tweets-container').append(createTweetElement(tweet))
+  }
+};
+
 
 // WHEN DOM is loaded and ready
 $(document).ready(() => {
 
-// Template literal
-  const createTweetElement = function(tweetData) {
-
-    const $tweet = $('<article>').addClass('tweet');
-    const timeSinceTweet = (Date.now() - tweetData.created_at) / 86400000;
-
-    const htmlTemplate = `
-      <header>
-        <img src=${tweetData.user.avatars} alt="${tweetData.user.handle}-avatar">
-        <span class="name" >${tweetData.user.name}</span>
-        <span class="handle" >${tweetData.user.handle}</span>
-      </header>
-      <p>${tweetData.content.text}</p>
-      <footer>
-        ${Math.round(timeSinceTweet)} days ago
-        <span>
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-solid fa-retweet"></i>
-          <i class="fa-solid fa-heart"></i>
-        </span>
-      </footer>
-    `
-    // Input the inner HTML into the tweet and return it
-    return $tweet.html(htmlTemplate);
-  };
+  // Event listener for submit and prevents its default behaviour
+  $('.new-tweet form').submit(evt => {
+    evt.preventDefault();
+  })
 
 
-  // Takes in an array of tweet objects and then appends each one to the #tweets-container
-  const renderTweets = function(tweets) {
-    for (const tweet of tweets) {
-      $('#tweets-container').append(createTweetElement(tweet))
-    }
-  };
 
 
-  // TEST
+
+
+// TEST
   const data = [
     {
       "user": {
