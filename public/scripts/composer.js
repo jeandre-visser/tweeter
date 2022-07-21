07afecbd631aa7c8f8e182aad5de86dabcb317c6
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-21 20:46:17 +0000

    Changed hide and show to fadeIn and fadeOut

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -6,12 +6,12 @@ $(document).ready(() => {
     // conditions for whether at top of page or not, determines if navbar or scroll up button is shown
 
     if ($(this).scrollTop() < 90) {
-      $('nav').show(100);
-      $('.scroll-up').hide(200);
+      $('nav').fadeIn(200);
+      $('.scroll-up').fadeOut(200);
 
     } else {
-      $('nav').hide(100);
-      $('.scroll-up').show(200);
+      $('nav').fadeOut(100);
+      $('.scroll-up').fadeIn(200);
     }
   })
 
