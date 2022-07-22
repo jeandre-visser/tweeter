2282ac42ab1bee7329dc85dad78d9ed7aa5976c9
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-22 03:09:54 +0000

    Eslint edits

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,13 @@
 // When DOM finishes loading
 $(document).ready(function() {
   // input box for tweeting
-  const $textArea = $('.new-tweet textarea')
+  const $textArea = $('.new-tweet textarea');
 
   // use keyup to increase char counter every time text is inputted
   $textArea.on('keyup', function() {
 
     // accesses the counter sibling of textArea
-    const $charCounter = $('.new-tweet .counter')
+    const $charCounter = $('.new-tweet .counter');
 
     // updates char counter when text is inputted
     const charCountRemaining = 140 - $(this).val().length;
@@ -22,5 +22,5 @@ $(document).ready(function() {
     } else {
       $charCounter.removeClass('char-limit-exceeded');
     }
-  })
+  });
 });
